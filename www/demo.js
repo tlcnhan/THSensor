6e4c02f18526b9195507671cdb8edf2a2a69eee8
tlcnhan <tlcnhan@gmail.com>
2020-02-22 11:20:45 -0500

    Web server

---
+++ www/demo.js
@@ -0,0 +1,102 @@
+// download lock monitoring data
+function downloadText() {
+var textFile = null,
+  makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    return textFile;
+  };
+
+    textbox = document.getElementById('messages');
+
+    var link = document.createElement('a');
+    link.setAttribute('download', 'info.txt');
+    link.href = makeTextFile(textbox.innerText);
+    document.body.appendChild(link);
+
+    // wait for the link to be added to the document
+    window.requestAnimationFrame(function () {
+      var event = new MouseEvent('click');
+      link.dispatchEvent(event);
+      document.body.removeChild(link);
+    });
+    
+}
+///////////////////////////////////////////////////////////////////////////////////
+
+// Called after form input is processed
+function startConnect() {
+    // Generate a random client ID
+    clientID = "NhanIOT";
+
+    // Fetch the hostname/IP address and port number from the form
+    host = document.getElementById("host").value;
+    port = document.getElementById("port").value;
+
+    // Print output for the user in the messages div
+    document.getElementById("messages").innerHTML += '<span>Connecting to: ' + host + ' on port: ' + port + '</span><br/>';
+    document.getElementById("messages").innerHTML += '<span>Using the following client value: ' + clientID + '</span><br/>';
+
+    // Initialize new Paho client connection
+    client = new Paho.MQTT.Client(host, Number(port), clientID);
+
+    // Set callback handlers
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    // Connect the client, if successful, call onConnect function
+    client.connect({ 
+        onSuccess: onConnect,    
+    });
+    }
+
+
+// Called when the client connects
+function onConnect() {
+    // Fetch the MQTT topic from the form
+    topic = document.getElementById("topic").value;
+
+    // Print output for the user in the messages div
+    document.getElementById("messages").innerHTML += '<span>Subscribing to: ' + topic + '</span><br/>';
+
+    // Subscribe to the requested topic
+    client.subscribe(topic);
+}
+
+// Called when the client loses its connection
+function onConnectionLost(responseObject) {
+    console.log("onConnectionLost: Connection Lost");
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost: " + responseObject.errorMessage);
+    }
+}
+
+// Called when a message arrives
+function onMessageArrived(message) {
+    console.log("onMessageArrived: " + message.payloadString);
+    document.getElementById("messages").innerHTML += '<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>';
+    updateScroll(); // Scroll to bottom of window
+}
+
+// Called when the disconnection button is pressed
+function startDisconnect() {
+    client.disconnect();
+    document.getElementById("messages").innerHTML += '<span>Disconnected</span><br/>';
+    updateScroll(); // Scroll to bottom of window
+}
+
+// Updates #messages div to auto-scroll
+function updateScroll() {
+    var element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
+}
+
+
