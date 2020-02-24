7a5f24915913259191a37303f4b4bd551b01b614
tlcnhan <34175170+tlcnhan@users.noreply.github.com>
2020-02-24 11:52:26 -0500

    save to csv file from data received  with the GUI

--- sensor_gui.py
+++ sensor_gui.py
@@ -2,6 +2,8 @@ import time
 import datetime as dt
 from tkinter import *
 import paho.mqtt.client as mqtt
+import os
+import csv
 # Main Tkinter application
 class Application(Frame):	
 	
@@ -34,12 +36,14 @@ class Application(Frame):
 		self.pack()
 		self.timer()
 		
+	# show current time	
 	def timer(self):
 		self.time.set(str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 		self.cur_time.pack()
 		# update time every second
 		self.after(1000, self.timer)
 		
+	# update data received from MQTT broker	
 	def receiveData_t1(self, t_data):
 		self.temp_data1.set("Temperature SHTC3  : " + t_data+ " oC")
 		self.temperature1.pack()
@@ -49,16 +53,46 @@ class Application(Frame):
 	def receiveData_t2(self, t_data):
 		self.temp_data2.set("Temperature TMP117: " + t_data + " oC")
 		self.temperature2.pack()	
+	
+	# write to csv file
+	def write_csv_data(self, filename, data, time_data):
+		with open(filename, mode = 'a') as f:
+			f_writer = csv.writer(f, delimiter = ',')
+			f_writer.writerow([data, time_data])
+
+	def write_csv_header(self, filename, header1, header2):
+		with open(filename, mode = 'a') as f:
+			f_writer = csv.writer(f, delimiter = ',')
+			f_writer.writerow([header1, header2])
 #########################################################################
 # MAIN 
 # receive temperature and humidity from sensors through MQTT in real time
 ##########################################################################
 app = Application()
-# Our "on message" event
+
+csv_filename_t_tmp117 = "temperature_TMP117.csv"
+csv_filename_t_shtc3 = "temperature_SHTC3.csv"
+csv_filename_h_shtc3 = "humidity_SHTC3.csv"
+header1 = 'temperature'
+header2 = 'humidity'
+header3 = 'time'
+
+# prepare header for csv file for temperature TMP117
+if not(os.path.isfile(csv_filename_t_tmp117)):
+    app.write_csv_header(csv_filename_t_tmp117, header1, header3)
+# prepare header for csv file for temperature SHTC3
+if not(os.path.isfile(csv_filename_t_shtc3)):
+    app.write_csv_header(csv_filename_t_shtc3, header1, header3)
+# prepare header for csv file for himidity SHTC3	
+if not(os.path.isfile(csv_filename_h_shtc3)):
+	app.write_csv_header(csv_filename_h_shtc3, header2, header3)	
+	
+# receive data from MQTT broker and update these values
 def messageFunction_t1 (client, userdata, message):
 	topic = str(message.topic)
 	message = str(message.payload.decode("utf-8"))
 	app.receiveData_t1(message)
+	app.write_csv_data(csv_filename_t_shtc3, message, str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 	
 client_t1 = mqtt.Client("Client_SHTC3_t") 
 client_t1.connect("test.mosquitto.org", 1883) 
@@ -66,11 +100,12 @@ client_t1.subscribe("NhanIOT/test/t_data_SHTC3/")
 client_t1.on_message = messageFunction_t1 # Attach the messageFunction to subscription
 client_t1.loop_start() # Start the MQTT client
 
-# Our "on message" event
+# receive data from MQTT broker and update these values
 def messageFunction_h1 (client, userdata, message):
 	topic = str(message.topic)
 	message = str(message.payload.decode("utf-8"))
 	app.receiveData_h1(message)
+	app.write_csv_data(csv_filename_h_shtc3, message, str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 	
 client_h1 = mqtt.Client("Client_SHTC3_h") 
 client_h1.connect("test.mosquitto.org", 1883) 
@@ -78,11 +113,12 @@ client_h1.subscribe("NhanIOT/test/h_data_SHTC3/")
 client_h1.on_message = messageFunction_h1 # Attach the messageFunction to subscription
 client_h1.loop_start() # Start the MQTT client
 
-# Our "on message" event
+# receive data from MQTT broker and update these values
 def messageFunction_t2 (client, userdata, message):
 	topic = str(message.topic)
 	message = str(message.payload.decode("utf-8"))
 	app.receiveData_t2(message)
+	app.write_csv_data(csv_filename_t_tmp117, message, str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 	
 client_t2 = mqtt.Client("Client_TMP117") 
 client_t2.connect("test.mosquitto.org", 1883) 
