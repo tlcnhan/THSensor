c1791ce749e2426f7fdc02c935f8625126667be9
tlcnhan <34175170+tlcnhan@users.noreply.github.com>
2020-02-24 11:23:17 -0500

    add timer to the GUI

--- sensor_gui.py
+++ sensor_gui.py
@@ -1,4 +1,5 @@
 import time
+import datetime as dt
 from tkinter import *
 import paho.mqtt.client as mqtt
 # Main Tkinter application
@@ -6,13 +7,17 @@ class Application(Frame):
 	
 	# Create display elements
 	def createWidgets(self):
-		self.temperature = Label(self, textvariable=self.temp_data, font=('Verdana', 20, 'bold'))
-		self.temp_data.set("Temperature SHTC3")
-		self.temperature.pack()
+		self.cur_time = Label(self, textvariable=self.time, font=('Verdana', 20, 'bold'))
+		self.time.set("Time")
+		self.cur_time.pack() # organize in block
 		
-		self.humidity = Label(self, textvariable=self.hum_data, font=('Verdana', 20, 'bold'))
-		self.hum_data.set("Humidity SHTC3")
-		self.humidity.pack()
+		self.temperature1 = Label(self, textvariable=self.temp_data1, font=('Verdana', 20, 'bold'))
+		self.temp_data1.set("Temperature SHTC3")
+		self.temperature1.pack() # organize in block
+		
+		self.humidity1 = Label(self, textvariable=self.hum_data1, font=('Verdana', 20, 'bold'))
+		self.hum_data1.set("Humidity SHTC3")
+		self.humidity1.pack()
 		
 		self.temperature2 = Label(self, textvariable=self.temp_data2, font=('Verdana', 20, 'bold'))
 		self.temp_data2.set("Temperature TMP117")
@@ -21,21 +26,28 @@ class Application(Frame):
 	# Init the variables & start measurements
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
-		
-		self.temp_data = StringVar()
-		self.hum_data = StringVar()
+		self.time = StringVar() # used for widget text edit
+		self.temp_data1 = StringVar()
+		self.hum_data1 = StringVar()
 		self.temp_data2 = StringVar()
 		self.createWidgets()
 		self.pack()
-
+		self.timer()
+		
+	def timer(self):
+		self.time.set(str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+		self.cur_time.pack()
+		# update time every second
+		self.after(1000, self.timer)
+		
 	def receiveData_t1(self, t_data):
-		self.temp_data.set("Temperature SHTC3  : " + t_data)
-		self.temperature.pack()
+		self.temp_data1.set("Temperature SHTC3  : " + t_data+ " oC")
+		self.temperature1.pack()
 	def receiveData_h1(self, h_data):
-		self.hum_data.set("Humidity SHTC          : " + h_data)
-		self.humidity.pack()	
+		self.hum_data1.set("Humidity SHTC3         : " + h_data+ " %")
+		self.humidity1.pack()	
 	def receiveData_t2(self, t_data):
-		self.temp_data2.set("Temperature TMP117: " + t_data)
+		self.temp_data2.set("Temperature TMP117: " + t_data + " oC")
 		self.temperature2.pack()	
 #########################################################################
 # MAIN 
@@ -47,7 +59,7 @@ def messageFunction_t1 (client, userdata, message):
 	topic = str(message.topic)
 	message = str(message.payload.decode("utf-8"))
 	app.receiveData_t1(message)
-
+	
 client_t1 = mqtt.Client("Client_SHTC3_t") 
 client_t1.connect("test.mosquitto.org", 1883) 
 client_t1.subscribe("NhanIOT/test/t_data_SHTC3/") 
@@ -59,7 +71,7 @@ def messageFunction_h1 (client, userdata, message):
 	topic = str(message.topic)
 	message = str(message.payload.decode("utf-8"))
 	app.receiveData_h1(message)
-
+	
 client_h1 = mqtt.Client("Client_SHTC3_h") 
 client_h1.connect("test.mosquitto.org", 1883) 
 client_h1.subscribe("NhanIOT/test/h_data_SHTC3/") 
@@ -71,7 +83,7 @@ def messageFunction_t2 (client, userdata, message):
 	topic = str(message.topic)
 	message = str(message.payload.decode("utf-8"))
 	app.receiveData_t2(message)
-
+	
 client_t2 = mqtt.Client("Client_TMP117") 
 client_t2.connect("test.mosquitto.org", 1883) 
 client_t2.subscribe("NhanIOT/test/t_data_TMP117/") 
